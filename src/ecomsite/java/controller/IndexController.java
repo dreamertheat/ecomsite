fbd0d790103b4106dc7e9687701b530a772d8fdf
Santus Ignis James <jamesreubengruta@gmail.com>
2019-10-18 00:07:55 +0800

    index works

--- src/ecomsite/java/controller/IndexController.java
+++ src/ecomsite/java/controller/IndexController.java
@@ -1,7 +1,11 @@
 package ecomsite.java.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
@@ -10,5 +14,14 @@ public class IndexController {
 	public String index() {
 		return "index";
 	}
+	
+	@RequestMapping("/about")
+	public ModelAndView about() {
+		ModelAndView view = new ModelAndView("about");
+			Map<String,String> map = new HashMap<>();
+			map.put("about", "some text about");
+			view.addAllObjects(map);
+			return view;
+		}
 	
 }
