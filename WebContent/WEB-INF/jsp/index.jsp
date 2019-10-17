36b9aeeaa0786f9465ac754bc94e2db049e88b81
Santus Ignis James <jamesreubengruta@gmail.com>
2019-10-18 00:48:45 +0800

    added date in index.jsp  via HttpSession

--- WebContent/WEB-INF/jsp/index.jsp
+++ WebContent/WEB-INF/jsp/index.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+   <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,7 @@
 <title>Test Page</title>
 </head>
 <body>
-	Hello World.!
+	<b>Hello!</b><br> 
+	The Current time: <c:out value="${time}"></c:out>
 </body>
 </html>
--- src/ecomsite/java/controller/IndexController.java
+++ src/ecomsite/java/controller/IndexController.java
@@ -1,8 +1,13 @@
 package ecomsite.java.controller;
 
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.apache.tomcat.jni.Time;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,7 +16,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 	
 	@RequestMapping("/")
-	public String index() {
+	public String index(HttpSession session) {
+		session.setAttribute("time", ""+new Timestamp(new Date().getTime())) ;
 		return "index";
 	}
 	
